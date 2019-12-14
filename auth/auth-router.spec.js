6a844baacc3761d45fb85ed92feec0a03aabaed7
AidenBow <AidenBow@users.noreply.github.com>
2019-12-13 20:21:12 -0800

    login tests

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -6,13 +6,11 @@ describe("auth-router-model", () => {
     expect(process.env.DB_ENV).toBe("testing")
   })
 
-  describe("add", () => {
+  describe("register", () => {
     it("should register a new user to the db", async () => {
-
       await Users.add({username: "aiden", password: "123"})
 
       const users = await db("users")
-
       expect(users).toHaveLength(1)
     })
 
@@ -24,6 +22,41 @@ describe("auth-router-model", () => {
       let user = await db("users")
       expect(user[0].username).toBe("aiden")
     })
+
+    beforeEach(async () => {
+      await db("users").truncate()
+    })
+  })
+
+  describe("login", () => {
+    it("should find all users", async () => {
+      await Users.add({
+        username: "aiden",
+        password: "123"
+      })
+      await Users.add({
+        username: "joker",
+        password: "123"
+      })
+
+      let users = await Users.find()
+      expect(users).toHaveLength(2)
+    })
+
+    it("should find a specific user", async () => {
+      await Users.add({
+        username: "aiden",
+        password: "123"
+      })
+      await Users.add({
+        username: "joker",
+        password: "123"
+      })
+
+      let user = await Users.findBy({username: "joker"}).first()
+      expect(user.username).toBe("joker")
+    })
+
 
     beforeEach(async () => {
       await db("users").truncate()
